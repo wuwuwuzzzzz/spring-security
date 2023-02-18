b5aa0ea6db98d1b9c6d709e239a1dadd41e192fd
wxz <759126349@qq.com>
2023-02-18 14:08:47 +0800

    优化代码

--- src/main/java/com/example/springsecurity/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/example/springsecurity/filter/JwtAuthenticationTokenFilter.java
@@ -1,9 +1,23 @@
 package com.example.springsecurity.filter;
 
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  *
  * @author wxz
  * @date 14:05 2023/2/18
  */
-public class JwtAuthenticationTokenFilter {
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+        FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
