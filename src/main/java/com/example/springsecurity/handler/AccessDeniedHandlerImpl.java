7a16770e77cb066e0ecc0a12d845fe1c6c3f0c72
wxz <759126349@qq.com>
2023-02-19 15:04:17 +0800

    优化代码

---
+++ src/main/java/com/example/springsecurity/handler/AccessDeniedHandlerImpl.java
@@ -0,0 +1,9 @@
+package com.example.springsecurity.handler;
+
+/**
+ *
+ * @author wxz
+ * @date 15:04 2023/2/19
+ */
+public class AccessDeniedHandlerImpl {
+}
---
+++ src/main/java/com/example/springsecurity/handler/AuthenticationEntryPointImpl.java
@@ -0,0 +1,26 @@
+package com.example.springsecurity.handler;
+
+import com.alibaba.fastjson.JSON;
+import com.example.springsecurity.domain.ResponseResult;
+import com.example.springsecurity.utils.WebUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author wxz
+ * @date 14:53 2023/2/19
+ */
+@Component
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) {
+        WebUtils.renderString(response,
+            JSON.toJSONString(new ResponseResult(HttpStatus.UNAUTHORIZED.value(), "用户认证失败请查询登录")));
+    }
+}
